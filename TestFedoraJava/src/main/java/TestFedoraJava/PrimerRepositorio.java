fd29d3e585a87ec74d80266f23c3418ed3996b55
Juan Carlos Piña Tapia <tapia.jcarlos65@gmail.com>
2019-05-13 13:45:11 -0500

    Este es el primer repositorio de piña en java

---
+++ TestFedoraJava/src/main/java/TestFedoraJava/PrimerRepositorio.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestFedoraJava;
+
+/**
+ *
+ * @author jcarlos
+ */
+public class PrimerRepositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("Hola a todos");
+    }
+    
+}
